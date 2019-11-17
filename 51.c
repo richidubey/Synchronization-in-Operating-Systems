f8d8da3edd05eb8e925cc700184c6e4f6afca2a8
2018A7PS0113G <f20180113@goa.bits-pilani.ac.in>
2019-11-17 14:14:42 +0530

    Half done sleeping barber

--- 51.c
+++ 51.c
@@ -1,33 +1,34 @@
-
 #include<semaphore.h>
 #include<stdio.h>
+#include<pthread.h>
 
-sem_t sem;
-
-
-
-void* thread1(void* ptr)
+sem_t shopfull;
+//Shop full runs contradictory motion, if full, val would be 0, otherwise non zero.
+void* barber(void* ptr)
 {
-	int ret=ret=sem_wait(&sem);
-	
-	for(int i=0;i<100;i++)
-	printf("This is thread 1 executing");
+	int ret=sem_wait(&shopempty);		//barber waits if shop is empty. Basically he sleeps.
 	
 	
-	ret=sem_post(&sem);
+	
+	for(int i=0;i<50;i++)
+	printf("This is Barber\n");
+	
+	
+	ret=sem_post(&shopfull);			//Increases value of shop full
 	
 }
 
 
-void* thread2(void* ptr)
+void* customer(void* ptr)
 {
-	int ret=sem_wait(&sem);
+	int curr;
+	int ret=sem_wait(&shopfull);		//wait if shop is full.
 	
 		
-	for(int i=0;i<100;i++)
-	printf("This is thread 2 executing");
+	for(int i=0;i<50;i++)
+	printf("This is customer\n");
 	
-	ret=sem_post(&sem);	
+	ret=sem_post(&shopempty);		//Customer successfully enters, so signals barber to wake up if asleep. Otherwise stay awake until he gets served.	
 }
 
 
@@ -58,15 +59,24 @@ int main()
 	
 	//sem_post automatically increments the semaphore pointed to by sem. When any thread is blocked on semaphore, it gets unblocked.
 	
+	//ret=sem_trywait(&sem);
+	
+	//Atomically decrements the value of sem, this is non blocking version of sem_wait. So if the value is zero, it does not do anything.
 	
 	//ret=sem_wait(&sem);
 	
 	//sem_wait blocks the calling thread until the count in the thread becomes greater than zero, then it automatically decrements it.
 	
 	
-	pthread_t pid;
+	pthread_t pidc,pidb;
 	
-	pthread_attr_t
+	pthread_create(&pidb,NULL,barber,NULL);
+	
+	pthread_create(&pidc,NULL,customer,NULL);
+	
+	
+	pthread_join(pidb,NULL);	
+	pthread_join(pidc,NULL);
 	
 
 	return 0;
